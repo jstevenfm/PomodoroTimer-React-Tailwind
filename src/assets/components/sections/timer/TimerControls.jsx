eab2b15a7c7ec8ddf5038f143a0df38baed64dd4
Jeisson Fonseca <jstevenfm@gmail.com>
2025-03-27 18:19:58 -0500

    feat: implement BtnTimerControls component and update TimerControls to use it

--- src/assets/components/sections/timer/TimerControls.jsx
+++ src/assets/components/sections/timer/TimerControls.jsx
@@ -1,9 +1,15 @@
+import BtnTimerControls from "../../ui/BtnTimerControls";
+
 const TimerControls = () => {
+  const controls = [
+    { title: "Reset" },
+    { title: "Start" },
+    { title: "Next State" },
+  ]
+
   return (
     <div className="py-2 px-5 w-3/4 max-w-72 grid grid-cols-3 justify-items-center">
-      <button className="">Start</button>
-      <button className="">Stop</button>
-      <button className="">Reset</button>
+      {controls.map( control => <BtnTimerControls key={control.title} title={control.title} /> )}
     </div>
   );
 }
---
+++ src/assets/components/ui/BtnTimerControls.jsx
@@ -0,0 +1,8 @@
+const BtnTimerControls = ({ title }) => {
+  return (
+    <button className="cursor-pointer hover:scale-105" title={title}>{title}</button>
+
+  )
+}
+
+export default BtnTimerControls
