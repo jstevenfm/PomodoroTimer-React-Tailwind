a71960057ef9c9ce832a23c4b25ab94fc23ec971
Jeisson Fonseca <jstevenfm@gmail.com>
2025-05-07 21:08:53 -0500

    fix: remove unused timer prop from TimerControls component

--- src/assets/components/sections/timer/TimerControls.jsx
+++ src/assets/components/sections/timer/TimerControls.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 import BtnTimerControls from "../../ui/BtnTimerControls";
 
-const TimerControls = ({ timer, setTimer }) => {
+const TimerControls = ({ setTimer }) => {
   const [isActive, setIsActive] = useState(false);
 
   const controls = [
