b0ab4dee1ea90c83c597ef92685988d4c9ca596c
Jeisson Fonseca <jstevenfm@gmail.com>
2025-05-25 20:17:55 -0500

    feat: Change Cancel button to reset form in InputNewTask component

--- src/assets/components/sections/task/InputNewTask.jsx
+++ src/assets/components/sections/task/InputNewTask.jsx
@@ -21,7 +21,8 @@ const InputNewTask = ({ setTasks }) => {
     <form onSubmit={addNewTask} className="flex justify-between items-center bg-yellow-300 mt-5 rounded-xl p-4 gap-4">
       <input type="text" name="title" className="flex-2/3" />
         <button type="submit" >Add</button>
-        <button type="button">Cancel</button>
+        <button type="reset">Cancel</button>
+
     </form>
   )
 }
