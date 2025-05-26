8dcc35c08ac8058d54941a47aceb344b34a018c8
Jeisson Fonseca <jstevenfm@gmail.com>
2025-05-25 20:06:50 -0500

    feat: Integrate InputNewTask component into TasksHeader for task addition functionality

--- src/assets/components/sections/task/InputNewTask.jsx
+++ src/assets/components/sections/task/InputNewTask.jsx
@@ -1,8 +1,26 @@
-const InputNewTask = () => {
+const InputNewTask = ({ setTasks }) => {
+
+  const addNewTask = (e) => {
+    e.preventDefault();
+
+    const inputTitle = e.target.title.value;
+
+    if (inputTitle === "") {
+      return;
+    }
+    const newTask = {
+      id: Math.random().toString(36).substring(2, 9),
+      title: inputTitle,
+      completed: false,
+    };
+    setTasks((prevTasks) => [...prevTasks, newTask]);
+    e.target.title.value = "";
+  };
+
   return (
-    <form className="flex justify-between items-center bg-yellow-300 mt-5 rounded-xl p-4 gap-4">
-      <input type="text" className="flex-2/3" ></input>
-        <button type="submit">Add</button>
+    <form onSubmit={addNewTask} className="flex justify-between items-center bg-yellow-300 mt-5 rounded-xl p-4 gap-4">
+      <input type="text" name="title" className="flex-2/3" />
+        <button type="submit" >Add</button>
         <button type="button">Cancel</button>
     </form>
   )
--- src/assets/components/sections/task/TasksHeader.jsx
+++ src/assets/components/sections/task/TasksHeader.jsx
@@ -1,9 +1,19 @@
+
+
+import InputNewTask from "./InputNewTask";
+
 const TasksHeader = ({ setTasks }) => {
+
   return (
-    <header className="flex justify-between items-center">
-      <h2 className="text-xl font-medium">Tasks</h2>
-      <button className="cursor-pointer">Add Task</button>
-    </header>
+    <>
+      <header className="flex justify-between items-center">
+        <h2 className="text-xl font-medium">Tasks</h2>
+        <button className="cursor-pointer" >
+          Add Task
+        </button>
+      </header>
+      <InputNewTask setTasks={setTasks} />
+    </>
   );
 };
 
