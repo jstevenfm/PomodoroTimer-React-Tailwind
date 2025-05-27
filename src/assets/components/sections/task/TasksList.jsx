6a6c532e87a5b8195e0e3d1ecb4348ad86062b20
Jeisson Fonseca <jstevenfm@gmail.com>
2025-05-26 22:19:15 -0500

    feat: Update TasksList component to use label for task titles and improve accessibility

--- src/assets/components/sections/task/TasksList.jsx
+++ src/assets/components/sections/task/TasksList.jsx
@@ -8,8 +8,8 @@ const TasksList = ({ tasks, setTasks }) => {
     <ul>
       {tasks.map( task => (
         <li key={task.id} className="flex justify-between mt-5 bg-yellow-300 rounded-xl gap-3 p-3">
-          <input type="checkbox" onClick={() => handleCompleteTask(task.id)} className="cursor-pointer" />
-          <p className=" flex-1/2">{task.title}</p>
+          <input type="checkbox" id={task.title} name={task.title} onClick={() => handleCompleteTask(task.id)} className="cursor-pointer" />
+          <label htmlFor={task.title} className=" flex-1/2">{task.title}</label>
           <button className="cursor-pointer hover:scale-110">Delete</button>
         </li>
       ))}
