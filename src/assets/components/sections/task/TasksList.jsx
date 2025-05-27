00ebd6ba795746e58021f163dd64cb5ae6ecd040
Jeisson Fonseca <jstevenfm@gmail.com>
2025-05-26 22:15:22 -0500

    feat: Implement task completion toggle in TasksList component

--- src/assets/components/sections/task/TasksList.jsx
+++ src/assets/components/sections/task/TasksList.jsx
@@ -1,9 +1,14 @@
 const TasksList = ({ tasks, setTasks }) => {
+  
+  const handleCompleteTask = (id) => {
+    setTasks(tasks.map(task => task.id === id ? { ...task, completed: !task.completed } : task));
+  }
+
   return (
     <ul>
       {tasks.map( task => (
         <li key={task.id} className="flex justify-between mt-5 bg-yellow-300 rounded-xl gap-3 p-3">
-          <input type="checkbox" className="cursor-pointer" />
+          <input type="checkbox" onClick={() => handleCompleteTask(task.id)} className="cursor-pointer" />
           <p className=" flex-1/2">{task.title}</p>
           <button className="cursor-pointer hover:scale-110">Delete</button>
         </li>
