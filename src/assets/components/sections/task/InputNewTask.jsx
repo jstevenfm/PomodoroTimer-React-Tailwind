aa7f459fc08c719168c38d66ec0da4d5cce4466b
Jeisson Fonseca <jstevenfm@gmail.com>
2025-05-31 15:06:45 -0500

    feat: Replace button text with IconAcceptTask and IconCancelTask for improved UI

--- src/assets/components/sections/task/InputNewTask.jsx
+++ src/assets/components/sections/task/InputNewTask.jsx
@@ -1,3 +1,6 @@
+import IconAcceptTask from "../../icons/IconAcceptTask"
+import IconCancelTask from "../../icons/IconCancelTask"
+
 const InputNewTask = ({ setTasks, className, setShowInputNewTask }) => {
 
   const addNewTask = (e) => {
@@ -20,8 +23,12 @@ const InputNewTask = ({ setTasks, className, setShowInputNewTask }) => {
   return (
     <form onSubmit={addNewTask} className={`flex justify-between items-center bg-yellow-300 mt-5 rounded-xl p-4 gap-4 ${className}`}>
       <input type="text" name="title" className="flex-2/3" />
-        <button className="hover:scale-105 cursor-pointer" type="submit" >Add</button>
-        <button className="hover:scale-105 cursor-pointer" type="reset" onClick={() => setShowInputNewTask(false)}>Cancel</button>
+        <button title="Accept New Task" className="hover:scale-105 cursor-pointer" type="submit" >
+          <IconAcceptTask />
+        </button>
+        <button title="Cancel New Task" className="hover:scale-105 cursor-pointer" type="reset" onClick={() => setShowInputNewTask(false)}>
+          <IconCancelTask />
+        </button>
 
     </form>
   )
