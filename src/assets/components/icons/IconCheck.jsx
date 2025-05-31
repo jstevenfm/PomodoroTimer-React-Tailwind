31d982211a0262c90199f3aacdcbf771fe3db728
Jeisson Fonseca <jstevenfm@gmail.com>
2025-05-31 14:01:52 -0500

    feat: Add new icon components for enhanced UI elements

---
+++ src/assets/components/icons/IconCheck.jsx
@@ -0,0 +1,13 @@
+const IconCheck = () => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      fill="CurrentColor"
+    >
+        <path fill="CurrentColor" d="M20 10A10 10 0 1 1 10 0a10.01 10.01 0 0 1 10 10Zm-1.538 0A8.461 8.461 0 1 0 10 18.462 8.47 8.47 0 0 0 18.462 10Z"/>
+    </svg>
+  )
+}
+
+export default IconCheck
---
+++ src/assets/components/icons/IconChecked.jsx
@@ -0,0 +1,13 @@
+const IconChecked = () => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      fill="CurrentColor"
+    >
+      <path fill="CurrentColor" d="M14.39 7.148a.771.771 0 0 1 0 1.089L9.006 13.62a.77.77 0 0 1-1.089 0L5.61 11.313a.77.77 0 0 1 1.088-1.088l1.764 1.764 4.84-4.84a.77.77 0 0 1 1.088 0ZM20 10A10 10 0 1 1 10 0a10.01 10.01 0 0 1 10 10Zm-1.538 0A8.461 8.461 0 1 0 10 18.462 8.47 8.47 0 0 0 18.462 10Z"/>
+    </svg>
+  )
+}
+
+export default IconChecked
