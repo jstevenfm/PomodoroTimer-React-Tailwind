9939d6843a541c27657cb67531e8f28fde992915
Jeisson Fonseca <jstevenfm@gmail.com>
2025-05-31 14:54:16 -0500

    feat: Replace delete button text with IconTrash component for improved UI

--- src/assets/components/ui/TaskListItem.jsx
+++ src/assets/components/ui/TaskListItem.jsx
@@ -1,10 +1,14 @@
+import IconTrash from '../icons/IconTrash'
+
 const TaskListItem = ({ taskId, taskTitle, completed, onClickCheckbox, onClickDeleteBtn }) => {
   return (
 
         <li className="flex justify-between mt-5 bg-yellow-300 rounded-xl gap-3 p-3">
           <input type="checkbox" checked={completed} id={taskTitle} name={taskTitle} onChange={onClickCheckbox} className="cursor-pointer" />
           <label htmlFor={taskTitle} id={taskId} className=" flex-1/2">{taskTitle}</label>
-          <button onClick={onClickDeleteBtn} className="cursor-pointer hover:scale-110">Delete</button>
+          <button onClick={onClickDeleteBtn} className="cursor-pointer hover:scale-110">
+            <IconTrash />
+          </button>
         </li>
   )
 }
