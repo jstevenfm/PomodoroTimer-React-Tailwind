ec18fd4be20384aa53a736ad80ef7ff51e66447c
Jeisson Fonseca <jstevenfm@gmail.com>
2025-05-30 19:50:45 -0500

    fix: Remove unnecessary div wrapper for cleaner structure in ButtonRadioStatus component

--- src/assets/components/ui/BtnRadioStatus.jsx
+++ src/assets/components/ui/BtnRadioStatus.jsx
@@ -1,10 +1,8 @@
 const ButtonRadioStatus = ({id, text, onClick, defaultChecked }) => {
   return (
     <>
-      <div className="hover:scale-105"> {/* This div is just for styling purposes, you can remove it*/}
-        <input type="radio" id={id} name="status" className="w-2" onClick={onClick} defaultChecked={defaultChecked} />
+        <input type="radio" id={id} name="status" className="hidden" onClick={onClick} defaultChecked={defaultChecked} />
         <label htmlFor={id} className="px-2.5 cursor-pointer text-center hover:scale-105">{text}</label>
-      </div>
     </>
   )
 }
