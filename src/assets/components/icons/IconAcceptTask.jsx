29a83dde2db164df947386b72c912c038150c155
Jeisson Fonseca <jstevenfm@gmail.com>
2025-05-31 15:03:25 -0500

    feat: Add IconAcceptTask and IconCancelTask components for enhanced UI

---
+++ src/assets/components/icons/IconAcceptTask.jsx
@@ -0,0 +1,14 @@
+const IconAcceptTask = () => {
+  return (
+    <svg
+      width="32"
+      height="32"
+      fill="CurrentColor"
+      viewBox="0 0 256 256"
+    >
+      <path d="m229.66 77.66-128 128a8 8 0 0 1-11.32 0l-56-56a8 8 0 0 1 11.32-11.32L96 188.69 218.34 66.34a8 8 0 0 1 11.32 11.32Z"/>
+    </svg>
+  )
+}
+
+export default IconAcceptTask
