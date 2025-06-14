35c01c98ba7f2feb01c94853556fe50db603908c
Jeisson Fonseca <jstevenfm@gmail.com>
2025-06-13 22:46:13 -0500

    feat: Add IconCheckTask component for task completion indication

---
+++ src/assets/components/icons/IconCheckTask.jsx
@@ -0,0 +1,12 @@
+const IconCheckTask = () => {
+  return (
+    <svg width="16" height="12" fill="CurrentColor">
+      <path
+        fill="CurrentColor"
+        d="M15.292.836a1.23 1.23 0 0 0-2.009-.4L5.54 8.184 2.717 5.36A1.231 1.231 0 0 0 .975 7.102l3.693 3.692a1.23 1.23 0 0 0 1.741 0l8.616-8.616a1.23 1.23 0 0 0 .267-1.342Z"
+      />
+    </svg>
+  );
+};
+
+export default IconCheckTask
