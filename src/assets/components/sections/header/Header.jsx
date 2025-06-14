0c6d565a9e19216730f85f11ba30576d2ef83ec2
Jeisson Fonseca <jstevenfm@gmail.com>
2025-06-13 22:30:33 -0500

    feat: Update Header component styles for improved padding and layout consistency

--- src/assets/components/sections/header/Header.jsx
+++ src/assets/components/sections/header/Header.jsx
@@ -3,12 +3,12 @@ import Logo from '../../icons/Logo.jsx'
 
 const Header = () => {
   return (
-    <header className="flex justify-between w-full p-4">
-      <a className="flex cursor-pointer hover:scale-110" href="/">
+    <header className="flex justify-between w-full">
+      <a className="p-4 flex cursor-pointer hover:scale-110" href="/">
         <Logo />
         <h1 className="px-3 self-center text-2xl font-semibold">Pomodoro Timer</h1>
       </a>
-      <button title='Settings' className="cursor-pointer hover:scale-120">
+      <button title='Settings' className="p-4 cursor-pointer hover:scale-120">
         <IconSettings />
       </button>
     </header>
